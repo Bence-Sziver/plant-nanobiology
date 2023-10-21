ef732ad485078b5c5f56d02f23ce44d998dc0a53
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2023-10-21 20:09:37 +0200

    creating index.html prototype

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenus() {
+    if (document.getElementById("expandable-menu").style.visibility === "hidden") {
+        document.getElementById("expandable-menu").style.display = "visible";
+    }   else {
+        document.getElementById("expandable-menu").style.display = "hidden";
+    }
+}
