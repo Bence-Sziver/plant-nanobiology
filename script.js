76a55e246b615021fa8700a3e039e2ce62b31953
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2023-10-22 18:51:32 +0200

    styling index.html

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenus() {
-    if (document.getElementById("expandable-menu").style.visibility === "hidden") {
-        document.getElementById("expandable-menu").style.display = "visible";
-    }   else {
-        document.getElementById("expandable-menu").style.display = "hidden";
-    }
-}
