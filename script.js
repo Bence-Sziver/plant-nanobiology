6c5c68b70529f4b84beacbffda50e6c84bf34172
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2023-10-30 20:39:26 +0100

    add transition effect on banner image change

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ images[2] = "./res/abra.jpg";
 images[3] = "./res/CHT02_25_5.jpg";
 images[4] = "./res/gsnor_NO_5cP15.jpg";
 images[5] = "./res/Kontrol_cyc_2.png";
+let current = "./res/banner.png";
+let text;
 
 function setImg() {
     i = (i === images.length - 1) ? 0 : i + 1;
-    document.getElementById("bannerImg").src = images[i];
-    document.getElementById("bannerImg").style.transition = "src .5s ease-in";
-    let text = (i === 0) ? "Szárazságstressznek kitett Arabidopsis thaliana" : "Lorem ipsum dolor sit amet";
-    document.getElementById("bannerCaption").innerHTML = text;
+
+    document.getElementById("fadeImg").src = current;
+    document.getElementById("bannerImg").classList.add("transitioning-src");
+    setTimeout(() => {
+        document.getElementById("bannerImg").setAttribute('src', images[i]);
+        current = images[i];
+        document.getElementById("bannerImg").classList.remove("transitioning-src");
+        text = (i === 0) ? "Szárazságstressznek kitett Arabidopsis thaliana" : "Lorem ipsum dolor sit amet";
+        document.getElementById("bannerCaption").innerHTML = text;
+    }, 400);
 }
 
 function startTimer() {
