6f04f8668effcd2f67999e46c2852a793fab21fd
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2023-11-09 13:48:45 +0100

    Add colleague images

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let images = [];
 let i = 0;
-images[0] = "./res/banner.png";
-images[1] = "./res/7SAH7.jpg";
-images[2] = "./res/abra.jpg";
-images[3] = "./res/CHT02_25_5.jpg";
-images[4] = "./res/gsnor_NO_5cP15.jpg";
-images[5] = "./res/Kontrol_cyc_2.png";
-let current = "./res/banner.png";
+images[0] = "./res/kezdolap/banner.png";
+images[1] = "./res/kezdolap/7SAH7.jpg";
+images[2] = "./res/kezdolap/abra.jpg";
+images[3] = "./res/kezdolap/CHT02_25_5.jpg";
+images[4] = "./res/kezdolap/gsnor_NO_5cP15.jpg";
+images[5] = "./res/kezdolap/Kontrol_cyc_2.png";
+let current = "./res/kezdolap/banner.png";
 let text;
 
 function setImg() {
