7d989108fbe7e780f077f0ac19338597c8f60890
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2023-11-09 16:13:01 +0100

    Reformat documents

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let images = [];
 let i = 0;
 images[0] = "./res/kezdolap/banner.png";
-images[1] = "./res/kezdolap/7SAH7.jpg";
+images[1] = "./res/kezdolap/7sah7.jpg";
 images[2] = "./res/kezdolap/abra.jpg";
-images[3] = "./res/kezdolap/CHT02_25_5.jpg";
-images[4] = "./res/kezdolap/gsnor_NO_5cP15.jpg";
-images[5] = "./res/kezdolap/Kontrol_cyc_2.png";
+images[3] = "./res/kezdolap/cht02_25_5.jpg";
+images[4] = "./res/kezdolap/gsnor_no_5cp15.jpg";
+images[5] = "./res/kezdolap/kontrol_cyc_2.png";
 let current = "./res/kezdolap/banner.png";
-//let text;
+
+addEventListener('load', startTimer);
 
 function setImg() {
     i = (i === images.length - 1) ? 0 : i + 1;
 
-    document.getElementById("fadeImg").src = current;
-    document.getElementById("bannerImg").classList.add("transitioning-src");
+    document.querySelector("#fadeImg").src = current;
+    document.querySelector("#bannerImg").classList.add("transitioning-src");
     setTimeout(() => {
-        document.getElementById("bannerImg").setAttribute('src', images[i]);
+        document.querySelector("#bannerImg").setAttribute('src', images[i]);
         current = images[i];
-        document.getElementById("bannerImg").classList.remove("transitioning-src");
+        document.querySelector("#bannerImg").classList.remove("transitioning-src");
     }, 400);
 }
 
